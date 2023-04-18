2a113fc1bd897351b5626b06e2c933d7a3eb9a76
Pawarin Maion <pawarinmaion@MacBook-Air-khxng-Pawarin.local>
2023-04-18 22:05:19 +0700

    first

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
+import { NavigationAction, NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import * as Svg from 'react-native-svg';
 
-export default function App() {
+import Home from './src/Home';
+import Nextscreen from './src/Nextscreen';
+import Header from './src/Header';
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        // options={{ 
+        //   title:
+        //    <Image
+        //      style={{ width: 200, height: 50 }}
+        //      source={require('./src/img/sun-tornado.png')}
+        //      //resizeMode='contain'
+        //    />
+        //  }}
+
+        options={{
+          headerTitle: () => <Header name="คำนวณน้ำยาง" />,
+          headerStyle:{
+            height: 130,
+            //borderBottomLeftRadius: 30,
+            //borderBottomRightRadius: 30,
+            //backgroundColor: '#7FB3D5',
+            //shadowColor: '#000',
+            //elevation: 25
+          },
+          headerBackground: () => (
+            <Image
+              style={StyleSheet.absoluteFill}
+              source={require('./src/img/sun-tornado1.png')}
+            />
+          ),
+        }}
+      >
+      </Stack.Screen>
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <App/>
+    </NavigationContainer>
+  )
+}
---
+++ src/Nextscreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Nextscreen = () => {
+    return (
+        <View>
+            <Text>
+                Nextscreen
+            </Text>
+        </View>
+    )
+}
+
+export default Nextscreen;
